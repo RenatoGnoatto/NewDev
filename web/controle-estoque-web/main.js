9dd942b54b789e81ca680995ed0273d692c7530f
Renato Romancini Gnoatto <datodudu123@gmail.com>
2022-06-13 21:28:31 -0300

    código controle de estoque basicamente finalizado

--- web/controle-estoque-web/main.js
+++ web/controle-estoque-web/main.js
@@ -1,18 +1,178 @@
-const cars = []
+var cars = []
 const storage = localStorage
+var carEditing = null
 
+const onStorage = () => {
+  const wareHouse = storage.getItem('wareHouse')
+  return wareHouse ? JSON.parse(wareHouse) : []
+}
+cars = onStorage()
+
+const percentageBar = () => {
+  var totalCars = 0
+  cars.forEach(item => {
+    totalCars += +item.quantity
+  })
+
+  var totalPercentage = totalCars / 2
+
+  const elem = document.getElementById('myBar')
+  const width = totalPercentage
+  elem.style.width = `${width}%`
+  elem.innerHTML = `${width}% ${totalCars}/200`
+}
+
+percentageBar()
+
+const isWareHouseFull = initialQuantity => {
+  var carsOnStock = +initialQuantity.value
+
+  cars.forEach(item => {
+    carsOnStock += +item.quantity
+  })
+  if (carsOnStock > 200) {
+    return true
+  } else {
+    return false
+  }
+}
+
+const addingCar = car => {
+  var form = document.getElementById('quantityEditForm')
+  var initialCars = carEditing.getAttribute('identificador')
+  if (isWareHouseFull(document.getElementById('quantityEditInput')) == true) {
+    alert('A quantidade passa dos limites')
+    return
+  } else {
+    cars[initialCars].quantity +=
+      +document.getElementById('quantityEditInput').value
+    storage.setItem('wareHouse', JSON.stringify(cars))
+    form.remove()
+    createList()
+    percentageBar()
+  }
+}
+
+const subtractCar = car => {
+  var form = document.getElementById('quantityEditForm')
+  var initialCars = carEditing.getAttribute('identificador')
+  cars[initialCars].quantity -=
+    +document.getElementById('quantityEditInput').value
+  storage.setItem('wareHouse', JSON.stringify(cars))
+  form.remove()
+  createList()
+  percentageBar()
+}
+
+const onClickEdit = item => {
+  carEditing = item
+  const iten = item
+  var form = document.getElementById('quantityEditForm')
+  if (form) {
+    form.remove()
+  } else {
+    form = document.createElement('form')
+    const label = document.createElement('label')
+    label.innerHTML = 'Insira a quantidade a ser modificada'
+    form.setAttribute(
+      'style',
+      'background-color: #067d1070; width:60rem; height:10rem;'
+    )
+    form.setAttribute('id', 'quantityEditForm')
+
+    const input = document.createElement('input')
+    input.setAttribute('style', 'width:50rem; margin-top:1rem')
+    input.setAttribute('type', 'number')
+    input.setAttribute('id', 'quantityEditInput')
+
+    const divButton = document.createElement('div')
+
+    const addButton = document.createElement('button')
+    addButton.innerHTML = 'Add'
+    addButton.setAttribute('id', 'addButton')
+    addButton.setAttribute(
+      'style',
+      'width:5rem; height:2rem; border-radius:10rem; border:0; margin:0.5rem'
+    )
+    addButton.setAttribute('onclick', 'addingCar(this)')
+
+    const removeButton = document.createElement('button')
+    removeButton.innerHTML = 'Minus'
+    removeButton.setAttribute('id', 'removeButton')
+    removeButton.setAttribute(
+      'style',
+      'width:5rem; height:2rem; border-radius:10rem; border:0; margin:0.5rem'
+    )
+    removeButton.setAttribute('onclick', 'subtractCar(this)')
+
+    divButton.appendChild(addButton)
+    divButton.appendChild(removeButton)
+
+    form.appendChild(label)
+    form.appendChild(input)
+    form.appendChild(divButton)
+
+    document.getElementById('quantityEdit').appendChild(form)
+
+    const plusButton = document.getElementById('addButton')
+    const minusButton = document.getElementById('removeButton')
+
+    const car = item
+    return car
+  }
+}
+
+const span = identificador => {
+  const span = document.createElement('span')
+
+  const quantityEdit = document.createElement('i')
+  quantityEdit.setAttribute('class', 'fas fa-plus-minus')
+  quantityEdit.setAttribute('title', 'add or remove cars')
+  quantityEdit.setAttribute('identificador', `${identificador}`)
+  quantityEdit.setAttribute('onclick', `onClickEdit(this)`)
+  quantityEdit.setAttribute('style', 'cursor:pointer; margin-inline: 1rem;')
+
+  span.appendChild(quantityEdit)
+  return span
+}
+
+const createList = () => {
+  var ul = document.querySelector('ul')
+  if (ul) {
+    ul.remove()
+  }
+
+  ul = document.createElement('ul')
+  cars.forEach((item, index) => {
+    const li = document.createElement('li')
+    li.innerHTML = `Brand: ${item.brand};
+    Model: ${item.model};
+    Year: ${item.year};
+    Quantity: ${item.quantity}`
+
+    li.appendChild(span(index))
+    ul.appendChild(li)
+  })
+  document.getElementById('list').appendChild(ul)
+}
+
+createList()
 const register = event => {
   event.preventDefault()
 
   const car = {
     brand: document.getElementById('brand').value,
     model: document.getElementById('model').value,
-    year: document.getElementById('year').value
+    year: document.getElementById('year').value,
+    quantity: 0
   }
-
+  if (!car.brand || !car.model || !car.year) {
+    alert('ALL THE INPUTS NEED CONTENT')
+    return
+  }
   cars.push(car)
   storage.setItem('wareHouse', JSON.stringify(cars))
-  console.log('cars --->', cars)
+  createList()
   document.querySelector('form').reset()
 }
 
--- web/new-dev-PDW-1/main.js
+++ web/new-dev-PDW-1/main.js
@@ -1,84 +1,152 @@
-const storage = localStorage
-let cadastros = []
+var cars = [];
+const storage = localStorage;
+var carEditing = null;
 
-const cadastrosStorage = () => {
-  const storageContent = storage.getItem('informacoes')
-  return storageContent ? JSON.parse(storageContent) : []
-}
+const onStorage = () => {
+  const wareHouse = storage.getItem("wareHouse");
+  return wareHouse ? JSON.parse(wareHouse) : [];
+};
+cars = onStorage();
 
-const onClickEdit = (element) => {
-  console.log('ON CLICK EDIT', element)
-}
+const addingCar = (car) => {
+  var form = document.getElementById("quantityEditForm");
+  var initialCars = carEditing.getAttribute("identificador");
+  cars[initialCars].quantity +=
+    +document.getElementById("quantityEditInput").value;
+  console.log("df", cars);
+  storage.setItem("wareHouse", JSON.stringify(cars));
+  form.remove();
+  createList();
+};
 
-const onClickRemove = (element) => {
-  console.log('ON CLICK REMOVE', element)
-}
+const subtractCar = (car) => {
+  var form = document.getElementById("quantityEditForm");
+  var initialCars = carEditing.getAttribute("identificador");
+  cars[initialCars].quantity -=
+    +document.getElementById("quantityEditInput").value;
+  storage.setItem("wareHouse", JSON.stringify(cars));
+  form.remove();
+  createList();
+};
+
+const onClickEdit = (item) => {
+  carEditing = item;
+  const iten = item;
+  var form = document.getElementById("quantityEditForm");
+  if (form) {
+    form.remove();
+  } else {
+    form = document.createElement("form");
+    const label = document.createElement("label");
+    label.innerHTML = "Insira a quantidade a ser modificada";
+    form.setAttribute(
+      "style",
+      "background-color: #067d1070; width:60rem; height: 8rem"
+    );
+    form.setAttribute("id", "quantityEditForm");
+    form.setAttribute("action", "#");
+
+    const input = document.createElement("input");
+    input.setAttribute("style", "width:50rem; margin-top:1rem");
+    input.setAttribute("type", "number");
+    input.setAttribute("id", "quantityEditInput");
+
+    const divButton = document.createElement("div");
+
+    const addButton = document.createElement("button");
+    addButton.innerHTML = "Add";
+    addButton.setAttribute("id", "addButton");
+    addButton.setAttribute(
+      "style",
+      "width:5rem; height:2rem; border-radius:10rem; border:0; margin:0.5rem"
+    );
+    addButton.setAttribute("onclick", "addingCar(this)");
+
+    const removeButton = document.createElement("button");
+    removeButton.innerHTML = "Minus";
+    removeButton.setAttribute("id", "removeButton");
+    removeButton.setAttribute(
+      "style",
+      "width:5rem; height:2rem; border-radius:10rem; border:0; margin:0.5rem"
+    );
+    removeButton.setAttribute("onclick", "subtractCar(this)");
+
+    divButton.appendChild(addButton);
+    divButton.appendChild(removeButton);
+
+    form.appendChild(label);
+    form.appendChild(input);
+    form.appendChild(divButton);
+
+    document.getElementById("quantityEdit").appendChild(form);
+
+    const plusButton = document.getElementById("addButton");
+    const minusButton = document.getElementById("removeButton");
+
+    const car = item;
+    return car;
+  }
+};
 
 const span = (identificador) => {
-  const span = document.createElement('span')
-  const iconEdit = document.createElement('i')
-  iconEdit.setAttribute('class', 'fas fa-edit')
-  iconEdit.setAttribute('title', 'Editar')
-  iconEdit.setAttribute('style', 'cursor:pointer; margin-inline: 1rem;')
-  iconEdit.setAttribute('identificador', identificador)
-  iconEdit.setAttribute('onclick', 'onClickEdit(this)')
-  span.appendChild(iconEdit)
+  const span = document.createElement("span");
+  const editList = document.createElement("i");
+  editList.setAttribute("class", "fas fa-list");
+  editList.setAttribute("title", "in-out list");
 
-  const iconRemove = document.createElement('i')
-  iconRemove.setAttribute('class', 'fas fa-trash')
-  iconRemove.setAttribute('title', 'Remover')
-  iconRemove.setAttribute('style', 'cursor:pointer; margin-inline: 1rem;')
-  iconRemove.setAttribute('identificador', identificador)
-  iconRemove.setAttribute('onclick', 'onClickRemove(this)')
-  span.appendChild(iconRemove)
+  editList.setAttribute("identificador", `${identificador}`);
+  editList.setAttribute("onclick", `onClickList(this)`);
+  editList.setAttribute("style", "cursor:pointer; margin-inline: 1rem;");
 
+  const quantityEdit = document.createElement("i");
+  quantityEdit.setAttribute("class", "fas fa-plus-minus");
+  quantityEdit.setAttribute("title", "add or remove cars");
+  quantityEdit.setAttribute("identificador", `${identificador}`);
+  quantityEdit.setAttribute("onclick", `onClickEdit(this)`);
+  quantityEdit.setAttribute("style", "cursor:pointer; margin-inline: 1rem;");
 
-  return span
-}
+  span.appendChild(quantityEdit);
+  span.appendChild(editList);
+
+  return span;
+};
 
 const createList = () => {
-  const peoples = cadastrosStorage()
-
-  let ul = document.querySelector('ul')
-  ul ? ul.remove() : console.log('num tem')
-
-  ul = document.createElement('ul')
-
-  peoples.forEach((item, identificador) => {
-    const li = document.createElement('li')
-    li.innerHTML = `Nome: ${item.nome} Sobrenome: ${item.sobreNome}, Endereço: ${item.endereco} 
-    Complemento ${item.complementoEndereco} Telefone ${item.telefone} Email ${item.email}`
-
-
-    li.appendChild(span(identificador))
-    ul.appendChild(li)
-  })
-
-  document.getElementById('list-background').appendChild(ul)
-}
-
-createList()
-const setList = event => {
-  event.preventDefault()
-  const elemento = document.getElementsByTagName('input')
-
-  const cadastro = {
-    nome: document.getElementById('nome').value,
-    sobreNome: document.getElementById('sobrenome').value,
-    endereco: document.getElementById('endereco').value,
-    complementoEndereco: document.getElementById('complementoEndereco').value,
-    telefone: document.getElementById('telefone').value,
-    email: document.getElementById('email').value
+  var ul = document.querySelector("ul");
+  if (ul) {
+    ul.remove();
   }
 
-  cadastros.push(cadastro)
-  storage.setItem('informacoes', JSON.stringify(cadastros))
-  console.log('array cadastros ---->', cadastros)
+  ul = document.createElement("ul");
+  cars.forEach((item, index) => {
+    const li = document.createElement("li");
+    li.innerHTML = `Brand: ${item.brand};
+    Model: ${item.model};
+    Year: ${item.year};
+    Quantity: ${item.quantity}`;
 
-  document.querySelector('form').reset()
+    li.appendChild(span(index));
+    ul.appendChild(li);
+  });
+  document.getElementById("list").appendChild(ul);
+};
 
-  createList()
-}
+createList();
+const register = (event) => {
+  event.preventDefault();
 
-const addButton = document.getElementById('enviar')
-addButton.addEventListener('click', setList)
+  const car = {
+    brand: document.getElementById("brand").value,
+    model: document.getElementById("model").value,
+    year: document.getElementById("year").value,
+    quantity: 0,
+  };
+
+  cars.push(car);
+  storage.setItem("wareHouse", JSON.stringify(cars));
+  createList();
+  document.querySelector("form").reset();
+};
+
+const botaoDeAdicionar = document.getElementById("addBtn");
+botaoDeAdicionar.addEventListener("click", register);

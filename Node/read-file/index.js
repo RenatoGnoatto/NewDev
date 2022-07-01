e4113ace24c3a2d564f62ee91816b1f4f69f4950
Renato Romancini Gnoatto <datodudu123@gmail.com>
2022-06-30 21:28:36 -0300

    30/06

---
+++ Node/read-file/index.js
@@ -0,0 +1,56 @@
+const http = require('http')
+const URL = require('url')
+const fs = require('fs')
+const path = require('path')
+let users = require('./users.json')
+
+const getUsers = (request, response) => {
+  const { name, lastName, age, remove } = URL.parse(request.url, true).query
+  let message = ''
+
+  if (name) {
+    const user = {
+      name,
+      lastName,
+      age
+    }
+
+    if (remove) {
+      message = 'Registro removido com sucesso'
+      const found = users.filter(user => String(user.name) === String(name))
+      users = users.filter(user => String(user.name) !== String(name))
+
+      // valida se não encontrar usuário
+      if (!found.length) {
+        return response.end('Usuário não encontrado!')
+      }
+    } else {
+      message = 'Registro salvo com sucesso'
+      users.push(user)
+    }
+
+    fs.writeFile(
+      path.join(__dirname, 'users.json'),
+      JSON.stringify(users, null, 2),
+      error => {
+        if (error) return
+
+        console.log('Salvou o registro com sucesso')
+        response.end(
+          JSON.stringify({
+            status: message,
+            data: user
+          })
+        )
+      }
+    )
+  } else {
+    response.end(JSON.stringify(users))
+  }
+}
+
+const server = http.createServer(getUsers)
+
+server.listen(3001, () => {
+  console.log('API listening on http://localhost:3001')
+})
